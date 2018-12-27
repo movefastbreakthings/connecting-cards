36e99f3005fe08cc6e6392d98cb4e49b9f896ebb
Tom <tom.kirchhartz@dunckelfeld.de>
2018-12-27 22:16:32 +0100

    initial commit#

---
+++ app.js
@@ -0,0 +1,93 @@
+console.log('Draw a card!');
+
+var cards = document.querySelectorAll('.cards');
+var card = document.querySelectorAll('.card');
+console.log('cards', cards);
+console.log('card', card);
+
+var allTexts = [
+  "2 things I am doing to clean up the past:",
+  "3 reasons I am beautiful: ",
+  "3 things I admire in other people:",
+  "3 words that describe your anger: ",
+  "A word that best describes me:",
+  "How do you feel about where you come from?",
+  "I am afraid of ... ",
+  "I am most looking forward to... ",
+  "I am proud of... ",
+  "I feel loved when... ",
+  "I feel loving when...",
+  "I know a lot about..",
+  "I need more of this: ",
+  "I need more time for... ",
+  "I need to let go of...",
+  "I need to shake up... ",
+  "I really wish ........ would call me.",
+  "I wish I were more... ",
+  "I'm never... ",
+  "If only I could...",
+  "It would be a miracle if....",
+  "Less........ would be good. ",
+  "More........ would be good. ",
+  "My family described in one word:",
+  "My favourite feeling... ",
+  "My greatest strength is... ",
+  "My least favourite feeling...",
+  "The most encouraging thing someone could say to you rigth now would be:",
+  "The most recent compliment I received...",
+  "The talents I would like to develop further...",
+  "This charges me up... ",
+  "This drains me... ",
+  "This makes me relax, chill out and switch off:",
+  "This scares me... ",
+  "What are the 3 things you would do on your ideal day?",
+  "What are the opposite feelings of how you want to feel?",
+  "What can you do to feel more connected to the person you love the most?",
+  "What do you need to create space for? ",
+  "What do you wish you had courage for?",
+  "What I Love about being in Love...",
+  "What I still want to learn... ",
+  "What makes you feel real?",
+  "What people thank you for the most?",
+  "What would going wild look like for you?",
+  "Who is really inspiring you right now?",
+  "Who is the most helpful in you carrer right now?",
+  "You can be sure I will always be: "
+];
+
+var gameTexts = allTexts.slice(0);
+
+for(var z = 0; z < cards.length; z++) {
+    var elem = cards[z];
+    elem.onclick = function() {
+        console.log(gameTexts.length);
+        gameTexts = shuffle(gameTexts);
+        if(gameTexts.length > 0) {
+          cards[0].classList.remove('end');
+          cards[0].classList.remove('start');
+          var text = gameTexts.pop()
+          console.log(text);
+          card[0].innerHTML = text;
+        } else {
+          card[0].innerHTML = 'No more cards, play again?';
+          gameTexts = allTexts.slice(0);
+          cards[0].classList.add('end');
+        }
+        return false;
+    };
+}
+
+/**
+ * Shuffles array in place.
+ * @param {Array} a items An array containing the items.
+ */
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
