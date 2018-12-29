fa4751b85bb2ff5ecbfc1318fe24c39cafec9d8e
Tom <tom.kirchhartz@dunckelfeld.de>
2018-12-29 23:34:25 +0100

    testing offline service worker

--- assets/js/serviceworker.js
+++ assets/js/serviceworker.js
@@ -4,15 +4,15 @@ var cacheVersion = 1;
 var currentCache = {
   offline: 'offline-cache' + cacheVersion
 };
-const offlineUrl = '/offline.html';
+const offlineUrl = 'offline.html';
 
 this.addEventListener('install', event => {
   event.waitUntil(
     caches.open(currentCache.offline).then(function(cache) {
       return cache.addAll([
-          '/assets/css/main.css',
-          '/assets/css/normalize.css',
-          '/assets/js/app.js',
+          './assets/css/main.css',
+          './assets/css/normalize.css',
+          './assets/js/app.js',
           offlineUrl
       ]);
     })
